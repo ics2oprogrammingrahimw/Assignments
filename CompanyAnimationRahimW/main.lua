e83eb7eb06d43e3eac1ed668fdc1d591640137be
Rahim Williams <47784173+s437374@users.noreply.github.com>
2019-04-16 09:06:15 -0400

    ADDED

--- CompanyAnimationRahimW/main.lua
+++ CompanyAnimationRahimW/main.lua
@@ -7,10 +7,15 @@
 
 display.setStatusBar(display.HiddenStatusBar)
 
+
+-----------------------------------------------------------------------------------------
+----OBJECT CREATION
+-----------------------------------------------------------------------------------------
+
+
 local backgroundimage = display.newImageRect("Images/bkgCL.jpg", 3048, 1536)
 
-
-
+local text1
 
 
 local Woodhouse = display.newImageRect("Images/CompanyLogoRahimW.png", 200, 200)
@@ -19,12 +24,26 @@ Woodhouse.y = display.contentHeight/2
 
 Woodhouse.alpha = 0
 
-local function GrowWoodhouse(event)
 
-	Woodhouse:scale( 1.01, 1.01)
-	Woodhouse.alpha = Woodhouse.alpha + 0.05	
+----------------------------------------------------------------------------------------------
+-----LOCAL FUNCTIONS
+---------------------------------------------------------------------------------------------
+local function Text()
+	backgroundImage = display.newImageRect("Images/Stars.png", 3048, 1536)
+	text1 = display.newText( "Night Time Games", 499, 381, nil, 100)
 end
 
 
+local function GrowWoodhouse(event)
+
+	Woodhouse:scale( 1.025, 1.025)
+	Woodhouse:rotate(2,2)
+	Woodhouse.alpha = Woodhouse.alpha + 0.05	
+	timer.performWithDelay( 2000, Text)
+end
+
+-----------------------------------------------------------------------------------------
+----FUNTION CALLS
+-----------------------------------------------------------------------------------------
 
 Runtime:addEventListener("enterFrame", GrowWoodhouse)
